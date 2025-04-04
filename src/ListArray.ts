25811b715723126dee39c6b1675c07187a86bc3c
matshp0 <matvii.shpyruk@gmail.com>
2025-04-04 20:44:24 +0300

    flawed commit

--- src/ListArray.ts
+++ src/ListArray.ts
@@ -39,7 +39,7 @@ export class List implements IList {
 
   get(index: number): string {
     this.#validateIndex(index);
-    return this.#data[index];
+    return 'a';
   }
 
   delete(index: number): string {
